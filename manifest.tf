3ce7dee6400b0507044c57da71ece5ce95c3814d
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-10-29 15:13:15 +1100

    Update manifest.tf

--- manifest.tf
+++ manifest.tf
@@ -76,14 +76,17 @@ resource "harness_platform_environment" "example" {
   identifier = "Development"
   name       = "Development"
   org_id     = "default"
-  project_id = var.HARNESS_PROJECT_ID
+  project_id = ${var.HARNESS_PROJECT_ID}
   type       = "PreProduction"
-  description = "env description"
 
-  yaml = format(<<-EOT
+  ## ENVIRONMENT V2 Update
+  ## The YAML is needed if you want to define the Environment Variables and Overrides for the environment
+  ## Not Mandatory for Environment Creation nor Pipeline Usage
+
+  yaml = <<-EOT
   environment:
+    name: Development
+    identifier: Development
     tags: {}
     type: PreProduction
-  EOT)
-}
-
+  EOT
